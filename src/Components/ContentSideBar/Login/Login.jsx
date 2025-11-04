40399f0ef34bdc19dbb0820ecfc4af5760d2eaac
ducthuan109 <ducthuan1022@gmail.com>
2025-11-04 22:44:06 +0700

    update toast sign up

--- src/Components/ContentSideBar/Login/Login.jsx
+++ src/Components/ContentSideBar/Login/Login.jsx
@@ -1,26 +1,97 @@
 import InputCommon from "@components/InputCommon/InputCommon";
-import styles from './style.module.scss'
+import styles from "./style.module.scss";
 import Button from "@components/Button/Button";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useContext, useState } from "react";
+import { ToastContext } from "@/contexts/ToastProvider";
+
 function Login() {
+  const { container, title, boxRememberMe, button, lost } = styles;
+  const [isRegister, setIsRegister] = useState(false);
+  const {toast} = useContext(ToastContext)
 
-    const {container,title, boxRememberMe, button, lost} = styles;
-    return (
-        <div className={container}>
-            <div className={title}>SIGN IN</div>
-            <InputCommon label='Email' type='text' isRequired/>
-            <InputCommon label='Password' type='password' isRequired/>
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email format")
+        .required("Email is required"),
+      password: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .required("Password is required"),
+        cfmpassword: Yup.string().oneOf(
+            [Yup.ref('password'), null],
+            'Passwords must match'
+        )
+    }),
+    onSubmit: (values) => {
+      console.log("Form submitted:", values);
+    },
+  });
 
-            <div className={boxRememberMe}>
-                <input type="checkbox" />
-                <span>Remember me</span>
-            </div>
-            <div className={button}>
-                 <Button content='LOGIN' variant="primary" fullWidth/>
-            </div>
+  const handleToggle = () => {
+    setIsRegister(!isRegister);
+    formik.resetForm();
+  };
 
-            <div className={lost}>Lost your password?</div>
+  return (
+    <div className={container}>
+      <div className={title}>{isRegister ? "SIGN UP" : "SIGN IN"}</div>
+      <form onSubmit={formik.handleSubmit}>
+        <InputCommon
+          id="email"
+          label="Email"
+          type="text"
+          isRequired
+          formik={formik}
+        />
+
+        <InputCommon
+          id="password"
+          label="Password"
+          type="password"
+          isRequired
+          formik={formik}
+        />
+
+        {isRegister && (
+          <InputCommon
+            id="cfmpassword"
+            label="Confirm password"
+            type="password"
+            isRequired
+            formik={formik}
+          />
+        )}
+
+        {!isRegister && (
+          <div className={boxRememberMe}>
+            <input type="checkbox" />
+            <span>Remember me</span>
+          </div>
+        )}
+        <div className={button}>
+          <Button content={isRegister ? 'REGISTER' : 'LOGIN'} variant="primary" fullWidth type="submit"
+            onClick={() => toast.success('Success')}
+           />
         </div>
-    )
+      </form>
+      <Button
+        content={
+          isRegister ? "Already have an account" : "Dont have an account"
+        }
+        type="submit"
+        isPrimary={false}
+        style={{ marginTop: "10px" }}
+        onClick={handleToggle}
+      />
+        {!isRegister &&    <div className={lost}>Lost your password?</div>}
+    </div>
+  );
 }
 
 export default Login;
--- src/Components/InputCommon/InputCommon.jsx
+++ src/Components/InputCommon/InputCommon.jsx
@@ -1,31 +1,50 @@
-import { useState } from 'react';
-import styles from './Style.module.scss'
+import { useState } from "react";
+import styles from "./Style.module.scss";
 import { FaRegEye } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa6";
 
-function InputCommon({label, type,isRequired = false}) {
-    const {labelInput,boxInput, container, boxIcon} = styles
-    const [showPassword, setShowPassword] = useState(false);
+function InputCommon({ label, type, isRequired = false, ...props }) {
+  const { labelInput, boxInput, container, boxIcon, errMsg } = styles;
+  const [showPassword, setShowPassword] = useState(false);
 
-    const isPassword = type === 'password';
+  const {formik,id} = props;
 
-    const isShowPassword = type === 'password' && showPassword ? 'text' : type
-     const handleShowPassword = () => {
-        setShowPassword(!showPassword)
-     }
-    return ( 
-        <div className={container}>
-            <div className={labelInput}>{label} {isRequired && <span>*</span>}</div>
-            <div className={boxInput}>
-                <input type={isShowPassword} />
-                {isPassword && (
-                    <div className={boxIcon} onClick={handleShowPassword}>
-                        {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  const isPassword = type === "password";
+
+  const isShowPassword = type === "password" && showPassword ? "text" : type;
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const isErr = formik.touched[id] && formik.errors[id];
+  const messageErr = formik.errors[id];
+
+
+  return (
+    <div className={container}>
+      <div className={labelInput}>
+        {label} {isRequired && <span>*</span>}
+      </div>
+      <div className={boxInput}>
+        <input
+          type={isShowPassword}
+          {...props}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values[id]}
+        />
+        {isPassword && (
+          <div className={boxIcon} onClick={handleShowPassword}>
+            {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+          </div>
+        )}
+        {
+            isErr &&  <div className={errMsg}>{messageErr}</div>
+        }
+        
+      </div>
+    </div>
+  );
 }
 
 export default InputCommon;
